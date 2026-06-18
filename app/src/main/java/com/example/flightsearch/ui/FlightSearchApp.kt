9e7165600ccaf6b343c2c6d8d6485e55fc7e39b8
Aksingh098 <reheat8725@gmail.com>
2026-06-18 16:20:07 +0530

    made topAppBar

--- app/src/main/java/com/example/flightsearch/ui/FlightSearchApp.kt
+++ app/src/main/java/com/example/flightsearch/ui/FlightSearchApp.kt
@@ -1,7 +1,14 @@
 package com.example.flightsearch.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -10,11 +17,33 @@ fun FlightSearchApp(){
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
+            SearchTopAppBar()
+
 
         }
     ) {innerpadding->
+        Box(modifier = Modifier.padding(innerpadding))
 
     }
+
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun SearchTopAppBar(){
+    TopAppBar(
+        title = {
+            Text(
+                text = "Flight Search",
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
 
 }
 
